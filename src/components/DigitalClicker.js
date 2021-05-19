4d0a023f8190e4a6faec54ec1de028306d1b86da
Cole Flournoy <flournoy.cole@gmail.com>
2021-05-19 16:51:25 -0400

    commit tree bf59a0db70dcc18bb580d09505317fb22357085d

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,48 @@
 // Code DigitalClicker Component Here
+
+// import React from 'react'
+
+// export default class DigitalClicker extends Component {
+//   constructor(){
+//     super()
+//     this.state = {
+//       timesClicked: 0,
+//     }
+//   }
+
+//   handleClick = () => {
+//     this.setState(previousState => ({
+//       timesClicked: previousState.timesClicked + 1  
+//     }))
+//   }
+
+//   render(){
+//     return <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+//   }
+// }
+
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      timesClicked: 0,
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      timesClicked: prevState.timesClicked+1
+    }))
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+    );
+  }
+}
+
+export default DigitalClicker;
